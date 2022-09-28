4ef856ebb41ea20b5d03fa14d8ac412c1fd94570
Kevin Kotze <kevinkotze@gmail.com>
2022-09-28 17:00:32 +0200

    make garch functions tidyverse compliant

--- R/archTest.R
+++ R/archTest.R
@@ -6,16 +6,16 @@
 #' @examples
 
 
-"archTest" <- function(rtn,m=10){
 
-y=(rtn-mean(rtn))^2
-T=length(rtn)
-atsq=y[(m+1):T]
-x=matrix(0,(T-m),m)
-for (i in 1:m){
-x[,i]=y[(m+1-i):(T-i)]
-}
-md=lm(atsq~x)
-summary(md)
+"archTest" <- function(rtn, m = 10) {
+  y <- (rtn - mean(rtn)) ^ 2
+  T <- length(rtn)
+  atsq <- y[(m + 1):T]
+  x <- matrix(0, (T - m), m)
+  for (i in 1:m) {
+    x[, i] <- y[(m + 1 - i):(T - i)]
+  }
+  md <- lm(atsq ~ x)
+  summary(md)
 }
 
